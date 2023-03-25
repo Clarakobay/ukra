54c4d096e4a10afab9a93ee23bc93270edfa4f5a
Clarakobay <clarakobaydasilva@gmail.com>
2023-03-25 12:06:23 -0300

    ukra
    criado o modelo usuário e conexão

---
+++ ukrabar_projeto/controle/clientecontrole.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.Eventing.Reader;
+using System.Linq.Expressions;
+using System.Net.Mail;
+using System.Security.Principal;
+
+Public Class clientecontrole
+    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace controle
+{
+    internal class clientecontrole : usuario_modelo
+    {
+       public usuario_modelo ()
+        {
+
+        }
+        public int cadastrar (usuario_modelo modelo)
+        {// variavel de confirmação do banco
+            int valorcadastro;
+            try
+            {
+                string SQL = "INSERT INTO cliente(nome,gmail, fone) values (@nome,@gmail,@fone)"
+                    string[] campos = ("@nome", "@gmail", "@fone");
+                string[] valores = (modelo.NomeCliente, modelo.cliente, modelo.senha);
+            }catch (Exception ex)
+            throw new Excepcion(ex.Message);
+            }
+            return valorcadastro;
+
+        }
+    }    
+
+End Class
---
+++ ukrabar_projeto/controle/conexao.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Serialization;
+
+Public Class Class1
+    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using 
+
+namespace controle
+{ () referencis
+    public class conexao
+    {
+        static private string servidor = "localhost";
+        static private string bd = "ukrasystem";
+        static private string usuario = "root";
+        static private string senha = "";
+        static private string StrCon = "server + servidor ++;databese=" + db + ";user id" + usuario + "passeword" + senha;
+        public mySqlconecction getconexao(Strcon);
+        Return conn;
+
+
+    }
+
+    public int cadastrar(string[] campos string[] valores, string SQLinsert)
+        
+        }
+int registro = 1 -;
+try 
+
+}
+   conn = Getconexao();
+conn.Open();
+mysqlcomand cmd = new mysqlcomand(SQLInsert, CONN);
+cmd. comandType = comandType.text
+    cmd.ComandText= SQLInsert 
+    for(int i =0; valores.length; i++)
+{
+    cmd 
+}
+   
+    End Class
